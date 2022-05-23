93466e5113efa33d0b6252700e01bc304353f483
pskoulgi <pskoulgi@users.noreply.github.com>
2022-05-23 16:07:41 +0530

    Update map palette, add satellite basemap & tweak map load zoom, increase height of map div.

--- apps/onemap.py
+++ apps/onemap.py
@@ -8,7 +8,7 @@ import matplotlib
 def app():
     st.title("Open Natural Ecosystem LandCover Types")
 
-    m = geemap.Map(location=(21, 79), zoom=4.2)
+    m = geemap.Map(location=(21, 79), zoom=5.2)
     one = ee.Image(
         "projects/ee-open-natural-ecosystems/assets/finalSprint/globalModelGbtBiomeZonesOhe/prediction"
     )
@@ -17,18 +17,18 @@ def app():
     top1 = one.select("top1LabelNum").clipToCollection(india)
 
     legendDict = {
-          "agri_hiBioMass": matplotlib.colors.cnames["darkviolet"],
-          "agri_loBiomass": matplotlib.colors.cnames["violet"],
-                    "bare": matplotlib.colors.cnames["beige"],
-                   "built": matplotlib.colors.cnames["red"],
-                    "dune": matplotlib.colors.cnames["khaki"],
-                  "forest": matplotlib.colors.cnames["darkgreen"],
-                  "ravine": matplotlib.colors.cnames["fuchsia"],
-                  "saline": matplotlib.colors.cnames["lightsteelblue"],
-            "savanna_open": matplotlib.colors.cnames["yellow"],
-           "savanna_shrub": matplotlib.colors.cnames["orange"],
-        "savanna_woodland": matplotlib.colors.cnames["lightgreen"],
-           "water_wetland": matplotlib.colors.cnames["blue"]
+        "Agriculture (high biomass)": matplotlib.colors.cnames["black"],
+         "Agriculture (low biomass)": matplotlib.colors.cnames["black"],
+                              "Bare": matplotlib.colors.cnames["beige"],
+                             "Built": matplotlib.colors.cnames["black"],
+                              "Dune": matplotlib.colors.cnames["khaki"],
+                            "Forest": matplotlib.colors.cnames["black"],
+                            "Ravine": matplotlib.colors.cnames["fuchsia"],
+                            "Saline": matplotlib.colors.cnames["lightsteelblue"],
+                    "Savanna (open)": matplotlib.colors.cnames["yellow"],
+                   "Savanna (shrub)": matplotlib.colors.cnames["goldenrod"],
+                "Savanna (woodland)": matplotlib.colors.cnames["greenyellow"],
+                  "Water or wetland": matplotlib.colors.cnames["black"]
     }
 
     vis_params = {
@@ -37,7 +37,8 @@ def app():
         "palette": list(legendDict.values()),
     }
 
+    m.add_basemap("SATELLITE")
     m.addLayer(top1, vis_params, "ONE LandCover Types", True, 1)
-    m.add_legend(legend_title = "Legend", legend_dict = legendDict)
+    m.add_legend(legend_title = "Landcover types", legend_dict = legendDict)
 
-    m.to_streamlit()
+    m.to_streamlit(height = 768)
