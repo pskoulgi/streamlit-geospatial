034df2349cc2703d18740ffe6c141515bec501bc
pskoulgi <pskoulgi@users.noreply.github.com>
2024-03-14 17:50:13 +0530

    Move info text from sidebar in each page, to main panel in homepage.

--- app.py
+++ app.py
@@ -3,24 +3,53 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
+st.sidebar.title("GitHub")
 st.sidebar.info(
     """
+    [https://github.com/openlandcover/](https://github.com/openlandcover/)
     """
 )
 
-st.sidebar.title("Contact")
-st.sidebar.info(
+# st.sidebar.title("Contact")
+# st.sidebar.info(
+#     """
+#     """
+# )
+
+st.sidebar.title("Terms of Use")
+st.sidebar.markdown(
     """
+    This map data and analysis code are available for free and open public use, under [MIT License](https://mit-license.org/).
     """
 )
 
 st.title("Open Land Cover Mapping of India's Semi-arid Open Natural Ecosystems (ONE)")
 
+st.info("Explore the map from pages on the left sidebar.")
+
 st.markdown(
     """
-    
+    India is endowed with a diversity of terrestrial biomes. Barring
+    forests—areas of closed tree canopies—they comprise diverse
+    naturally open and often treeless habitats that support their own
+    rich and unique complement of plant and animal life. Further,
+    for centuries, such diversity has thrived alongside unique pastoral
+    and agro-pastoral communities that have shared space with nature
+    in this biome, while deriving identity, sustenance and livelihood from it.
+
+    Together, these Open Natural Ecosystems stretch across a vast
+    area comprising some 15% of the land area in India's semi-arid zone.
+    The threats they face are vast too. Not only are they
+    disregarded for their unique ecological, cultural and livelihood values,
+    but they are also targets of huge government schemes and programmes for
+    well-intentioned developmental purposes that lead to serious
+    unintended negative consequences.
+
+    A previous effort to map the extent of these ONEs can be seen
+    [here](https://mdm.users.earthengine.app/view/open-natural-ecosystems),
+    with its methods and data freely and publicly accessible
+    in this [publication](https://doi.org/10.1111/jbi.14471).
     """
 )
 
-st.info("Click on the left sidebar menu to explore the map.")
 
--- pages/02_Probabilistic_Map.py
+++ pages/02_Probabilistic_Map.py
@@ -7,29 +7,24 @@ import matplotlib
 
 st.set_page_config(layout="wide")
 
+st.sidebar.title("GitHub")
+st.sidebar.info(
+    """
+    [https://github.com/openlandcover/](https://github.com/openlandcover/)
+    """
+)
+
+# st.sidebar.title("Contact")
+# st.sidebar.info(
+#     """
+#     """
+# )
+
+st.sidebar.title("Terms of Use")
 st.sidebar.markdown(
     """
-    India is endowed with a diversity of terrestrial biomes. Barring
-    forests—areas of closed tree canopies—they comprise diverse
-    naturally open and often treeless habitats that support their own
-    rich and unique complement of plant and animal life. Further,
-    for centuries, such diversity has thrived alongside unique pastoral
-    and agro-pastoral communities that have shared space with nature
-    in this biome, while deriving identity, sustenance and livelihood from it.
-
-    Together, these Open Natural Ecosystems stretch across a vast
-    area comprising some 15% of the land area in India's semi-arid zone.
-    The threats they face are vast too. Not only are they
-    disregarded for their unique ecological, cultural and livelihood values,
-    but they are also targets of huge government schemes and programmes for
-    well-intentioned developmental purposes that lead to serious
-    unintended negative consequences.
-
-    A previous effort to map the extent of these ONEs can be seen
-    [here](https://mdm.users.earthengine.app/view/open-natural-ecosystems),
-    with its methods and data freely and publicly accessible
-    in this [preprint](https://www.essoar.org/doi/10.1002/essoar.10507612.1).
-"""
+    This map data and analysis code are available for free and open public use, under [MIT License](https://mit-license.org/).
+    """
 )
 
 def app():
