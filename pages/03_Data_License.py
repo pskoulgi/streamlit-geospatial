27cb3d4b2ccd92abef9dc81168cc973fa1bb950c
Pradeep Koulgi <pskoulgi@users.noreply.github.com>
2024-08-26 13:24:38 +0000

    Drop suggested citation.

--- pages/03_Data_License.py
+++ pages/03_Data_License.py
@@ -58,7 +58,7 @@ def app():
     
     st.write("# Source Code")
     st.write("The source code used to produce the maps is available in [a GitHub repository](https://github.com/openlandcover/one7types). The repository contains the code used to train the models and produce the maps. The code is available under the MIT License, which is a permissive open source license. While not required, we would appreciate if you attribute the source if and when you use these data or code.")
-    st.write("**Suggested citation:** Koulgi, PS & Madhusudan, MD (2024) Open Land Cover Mapping of India's Semi-arid Open Natural Ecosystems. https://one-india.streamlit.app/")
+    # st.write("**Sugge`sted citation:** Koulgi, PS & Madhusudan, MD (2024) Open Land Cover Mapping of India's Semi-arid Open Natural Ecosystems. https://one-india.streamlit.app/")
     
     st.write("## A word on known issues and limitations")
     st.write("""
